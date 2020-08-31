f0552cb0e5286dd2ee0cb6c8f9a6a140c150aac5
Dmitry <wodi220500@gmail.com>
2020-08-31 23:51:36 +0300

    adding firebase and register method

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
 
@@ -14,6 +15,22 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        
+        
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+        
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            if let e = error {
+                let alert = UIAlertController(title: "Error", message: e.localizedDescription, preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                self.present(alert, animated: true)
+                print(e.localizedDescription)
+            } else {
+                //Navigate to ChatViewController
+                self.performSegue(withIdentifier: "RegisterTiChat", sender: self)
+            }
+            }
+        }
     }
     
 }
